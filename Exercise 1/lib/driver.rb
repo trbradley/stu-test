f593d8ac4a1675fefc1ad13e35f2e7a405561505
Tom Bradley <bradley.tom.r@gmail.com>
2016-06-07 18:45:35 +0100

    Refactors current_infos method to private methods

--- Exercise 1/lib/driver.rb
+++ Exercise 1/lib/driver.rb
@@ -1,20 +1,34 @@
 require_relative 'location'
 
 class Driver
-  def initialize=(delivery, location_klass = Location)
-    @delivery = delivery
+  attr_reader :current_position, :current_delivery
+
+  def initialize(delivery = 'Empty', location_klass = Location)
+    @current_delivery = delivery
     @current_position = location_klass.new
   end
 
   def current_infos
-    picking_eta = current_delivery.picking_eta(current_position.latitude, current_position.longitude)
-    delivery_eta = current_delivery.delivery_eta(current_position.latitude, current_position.longitude)
-    { picking_eta: picking_eta, delivery_eta: delivery_eta, current_latitude: current_position.latitude, current_longitude: current_position.longitude }
+    update_position
+    {
+      picking_eta: picking_eta,
+      delivery_eta: delivery_eta,
+      current_latitude: current_latitude,
+      current_longitude: current_longitude
+    }
   end
 
-  # private
+  private
 
-  def new_position
+  def picking_eta
+    @current_delivery.picking_eta(current_latitude, current_longitude)
+  end
+
+  def delivery_eta
+    @current_delivery.delivery_eta(current_latitude, current_longitude)
+  end
+
+  def update_position
     @current_position = @current_position.new_location
   end
 
@@ -23,11 +37,6 @@ class Driver
   end
 
   def current_latitude
-    @current_latitude.latitude
+    @current_position.latitude
   end
-
-  # def current_position
-  #   # Make a call to Google API which returns an object with 'latitude' and 'longitude'.
-  #   Service::Google.get_position
-  # end
 end
